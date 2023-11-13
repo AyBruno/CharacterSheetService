2735a7953177c7bb179b212cc4c9beec3cff61b5
Adrian Bruno <abruno.heel@gmail.com>
2023-11-13 12:13:23 -0500

    enum containing damage types

--- src/main/java/dev/abruno/dnd_service/action/AttackType.java
+++ src/main/java/dev/abruno/dnd_service/action/AttackType.java
@@ -1,2 +1,5 @@
-package dev.abruno.dnd_service.action;public enum AttackType {
+package dev.abruno.dnd_service.action;
+
+public enum AttackType{
+    PIERCING, BLUDGEONING, SLASHING, FIRE, COLD, LIGHTNING, FORCE, POISON, PSYCHIC, RADIANT, ACID, NECROTIC, THUNDER;
 }
