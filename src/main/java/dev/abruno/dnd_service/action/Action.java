dc52ec72175e200a75a087bc2e9c78ec07d4fe5f
Adrian Bruno <abruno.heel@gmail.com>
2023-11-13 18:34:22 -0500

    Added fields and updated damageType

--- src/main/java/dev/abruno/dnd_service/action/Action.java
+++ src/main/java/dev/abruno/dnd_service/action/Action.java
@@ -1,4 +1,4 @@
 package dev.abruno.dnd_service.action;
 
-public record Action(int id, String name, String description, AttackType attackType) {
+public record Action(int id, String name, String description, DamageType damageType, boolean isHomebrew, int spellLevel) {
 }
