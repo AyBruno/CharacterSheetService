27a337819ec7da1e5f027f9fd67683dbe90b8930
Adrian Bruno <abruno.heel@gmail.com>
2023-11-13 18:44:50 -0500

    Added Stat table
    Completed definition of the stats table and moved to migration directory for flyway

--- src/main/resources/db/migration/V3__initStatTable.sql
+++ src/main/resources/db/migration/V3__initStatTable.sql
@@ -1,11 +1,10 @@
 CREATE TABLE Stats(
-    character_id Integer,
+    character_id INTEGER ,
     str TINYINT DEFAULT 0,
     dex TINYINT DEFAULT 0,
     con TINYINT DEFAULT 0,
     intl TINYINT DEFAULT 0,
     wis TINYINT DEFAULT 0,
     cha TINYINT DEFAULT 0,
-    UNIQUE (character_id),
     PRIMARY KEY (character_id)
 );
