4aac979cd3abef37f300101f7503089f0fe14beb
Adrian Bruno <abruno.heel@gmail.com>
2023-11-13 12:33:08 -0500

    Utility class to randomly pick from any enum

--- src/main/java/dev/abruno/dnd_service/util/RandomEnumGenerator.java
+++ src/main/java/dev/abruno/dnd_service/util/RandomEnumGenerator.java
@@ -1,2 +1,17 @@
-package dev.abruno.dnd_service.util;public class RandomEnumGenerator {
+package dev.abruno.dnd_service.util;
+
+import java.util.Random;
+
+public class RandomEnumGenerator<T extends Enum<T>> {
+    private static final Random rnd = new Random();
+    private final T[] values;
+
+    public RandomEnumGenerator(Class<T> e){
+        this.values = e.getEnumConstants();
+    }
+
+    public T getRandomValue(){
+        return values[rnd.nextInt(values.length)];
+    }
+
 }
